b99a07899f392ff701bacb1f6255185fc2c7296e
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-04-21 18:31:59 -0300

    feat: Inicializing server

---
+++ server.js
@@ -0,0 +1,22 @@
+const { Socket } = require('dgram');
+const net = require('net');
+
+const server = net.createServer();
+const port = 6379;
+const host = "127.0.0.1";
+
+server.on("connection", (Socket) => {
+    Socket.on("data", (data) => {
+        const reqData = data.toString();
+
+        Socket.write("res: " + reqData);
+    });
+
+    Socket.on("end", () => {
+        console.log("Client disconnected");
+    });
+});
+
+server.listen(port, host, () => {
+    console.log(`Server running at ${host} : ${port}`);
+});
